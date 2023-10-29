93ae8d6f34732f449b20fbf9d3a65b92d6e9ff99
Szymon-Krysztopolski <94224331+Szymon-Krysztopolski@users.noreply.github.com>
2023-10-29 09:14:55 +0100

    ResponseEntity

--- backend/src/main/java/com/main/backend/features/nodes/api/NodeController.java
+++ backend/src/main/java/com/main/backend/features/nodes/api/NodeController.java
@@ -2,6 +2,8 @@ package com.main.backend.features.nodes.api;
 
 import com.main.backend.features.nodes.dto.TreePartsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,8 @@ public class NodeController {
     }
 
     @GetMapping("/tree")
-    public TreePartsDTO getTree() { // todo return HttpResponse
-        return service.getTree();
+    public ResponseEntity<TreePartsDTO> getTree() { // todo return HttpResponse
+        //return ResponseEntity.ok(service.getTree());
+        return ResponseEntity.status(HttpStatus.OK).body(service.getTree());
     }
 }
