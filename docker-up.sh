c759802ce7c51184a5b4138d9bf33950e22101b7
Szymon-Krysztopolski <94224331+Szymon-Krysztopolski@users.noreply.github.com>
2023-10-30 15:57:37 +0100

    script fix

--- docker-up.sh
+++ docker-up.sh
@@ -3,16 +3,18 @@ source .env
 set -x
 
 # Docker Compose service names
-service_name="b4m-treenity"
 database_service_name="b4m-treenity-database-treenity-1"
 sql_content=$(< "init.sql")
 
+# Variable encoding fix
+POSTGRES_USER=$(echo "$POSTGRES_USER" | tr -d '\r')
 
 # Stop Docker services
-docker-compose -p "$service_name" stop "$@"
+docker compose stop "$@"
 
 # Rebuild and start Docker services
-docker-compose -p "$service_name" up --build -d "$@"
+docker compose up --build -d "$@"
 
 # Initialize database
+sleep 1 # Wait for docker of database
 docker exec -it "$database_service_name" psql -U "$POSTGRES_USER" -d postgres -c "$sql_content"
