4bfa6d76dac1c9b440bcc23cbd56a4dcc69bca34
Szymon-Krysztopolski <94224331+Szymon-Krysztopolski@users.noreply.github.com>
2023-10-30 16:01:02 +0100

    todo

--- docker-up.sh
+++ docker-up.sh
@@ -2,18 +2,21 @@
 source .env
 set -x
 
+# Stop Docker services
+docker compose stop "$@"
+
+# Rebuild and start Docker services
+docker compose up --build -d "$@"
+
+#-------------------------------------------------------------------------
+# TODO - check initialization of database
+
 # Docker Compose service names
 database_service_name="b4m-treenity-database-treenity-1"
 sql_content=$(< "init.sql")
 
 # Variable encoding fix
 POSTGRES_USER=$(echo "$POSTGRES_USER" | tr -d '\r')
-
-# Stop Docker services
-docker compose stop "$@"
-
-# Rebuild and start Docker services
-docker compose up --build -d "$@"
 
 # Initialize database
 sleep 1 # Wait for docker of database
