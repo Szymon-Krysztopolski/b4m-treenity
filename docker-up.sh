6fd925e22f3b4ce0970926f4b41e4c34fa98a95e
Szymon-Krysztopolski <94224331+Szymon-Krysztopolski@users.noreply.github.com>
2023-10-30 14:24:26 +0100

    initialization of database wit example values

--- docker-up.sh
+++ docker-up.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+source .env
 set -x
 
 # Docker Compose service names
 service_name="b4m-treenity"
-database_service_name="database-treenity"
+database_service_name="b4m-treenity-database-treenity-1"
+sql_content=$(< "docs/init.sql")
+
 
 # Stop Docker services
 docker-compose -p "$service_name" stop "$@"
@@ -11,3 +14,5 @@ docker-compose -p "$service_name" stop "$@"
 # Rebuild and start Docker services
 docker-compose -p "$service_name" up --build -d "$@"
 
+# Initialize database
+docker exec -it "$database_service_name" psql -U "$POSTGRES_USER" -d postgres -c "$sql_content"
