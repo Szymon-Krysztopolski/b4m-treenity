eb7013976e66f1e8ba2814ae00826652863d524c
Szymon-Krysztopolski <94224331+Szymon-Krysztopolski@users.noreply.github.com>
2023-11-03 10:08:20 +0100

    docker services

---
+++ test/mock-database.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+BASE_DIR="$(dirname "$(realpath "$0")")"
+source "$BASE_DIR"/../.env
+
+# Docker Compose service names
+database_service_name="treenity-database-container"
+sql_content=$(< "$BASE_DIR/init.sql")
+
+# Variable encoding fix
+POSTGRES_USER=$(echo "$POSTGRES_USER" | tr -d '\r')
+
+# Initialize database
+sleep 1 # Wait for docker of database
+docker exec -it "$database_service_name" psql -U "$POSTGRES_USER" -d postgres -c "$sql_content"
