5595f4a86679852ca5d70da66b7425f89a88a78f
Szymon-Krysztopolski <94224331+Szymon-Krysztopolski@users.noreply.github.com>
2023-12-13 11:11:34 +0100

    cors additional functions

--- backend/src/main/java/com/main/backend/configuration/CorsConfig.java
+++ backend/src/main/java/com/main/backend/configuration/CorsConfig.java
@@ -5,17 +5,35 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Configuration
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://127.0.0.1:3000",
-                        "http://localhost:3000",
-                        "https://127.0.0.1:3000",
-                        "https://localhost:3000")
+                .allowedOrigins(addPorts(
+                        getUrls("localhost", "127.0.0.1"),
+                        "", ":3000")
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+
+    private String[] addPorts(List<String> urls, String... ports) {
+        return Arrays.stream(ports)
+                .flatMap(port -> urls.stream().map(url -> url + port))
+                .toArray(String[]::new);
+    }
+
+    private List<String> getUrls(String... ips) {
+        return Arrays.stream(ips)
+                .flatMap(ip -> Arrays.stream(new String[]{
+                        "http://" + ip,
+                        "https://" + ip
+                }))
+                .collect(Collectors.toList());
     }
 }
